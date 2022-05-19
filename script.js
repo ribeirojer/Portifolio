c9a7f9756542d52d7d4f14e56a1aace607d3f42f
José Eduardo Ribeiro <100392655+ribeirojer@users.noreply.github.com>
2022-05-18 22:21:48 -0300

    menu scroll e rodapé atualizado

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ function myFun() {
             
     var ob = document.getElementById("raiz");
     var imagebutton = document.getElementById("img-dark-mode");
-    var terceirofundo = document.getElementById("segundasecao");
-
+    
     if(ob.classList.contains("dark-mode")) {
         imagebutton.src = 'assets/lua.png';
         ob.classList.remove("dark-mode");
-        terceirofundo.style.backgroundImage="url(assets/ffflux-claro.svg)";
     }
     else{
         imagebutton.src = 'assets/sol.png';
         ob.classList.add("dark-mode");
-        terceirofundo.style.backgroundImage="url(assets/ffflux-escuro.svg)";
     }
 }
 
@@ -27,4 +24,30 @@ $(function(){
 		t.add(matchedPara).addClass('active');
 		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
 	});
+});
+
+$(function() {
+    $(window).on("scroll", function() {
+        altura = 649;
+        if($(window).scrollTop() < altura) {
+            $(".bx-home").addClass("visualizando");
+        } else {
+            $(".bx-home").removeClass("visualizando");
+        }
+        if($(window).scrollTop() >= altura && $(window).scrollTop() < 2*altura) {
+            $(".bx-body").addClass("visualizando");
+        } else {
+            $(".bx-body").removeClass("visualizando");
+        }
+        if($(window).scrollTop() >= 2*altura && $(window).scrollTop() < 3*altura) {
+            $(".bx-desktop").addClass("visualizando");
+        } else {
+            $(".bx-desktop").removeClass("visualizando");
+        }
+        if($(window).scrollTop() >= 3*altura ) {
+            $(".bx-id-card").addClass("visualizando");
+        } else {
+            $(".bx-id-card").removeClass("visualizando");
+        }
+    });
 });
